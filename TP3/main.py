09c07160ff5087cac1254a978cda065bcde3b68e
Marc-Adrien <146423650+Marc-Adr@users.noreply.github.com>
2025-02-04 19:06:58 +0100

    Delete TP3/main.py

--- TP3/main.py
+++
@@ -1,5 +0,0 @@
-from TP3.search_engine import *
-
-synonym_country= open_json("TP3/origin_synonyms.json")
-token_exemple =tokenize_without_stopwords_punctuation("La france est en europe alors que les usa en amérique")
-expand_query(tokens=token_exemple, synonyms_dict=synonym_country)
